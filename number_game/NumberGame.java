89366db9b52a7c24a8a518062285328ff352daa1
himanshu3232 <himanshu2000ranjan@gmail.com>
2023-12-23 04:13:47 +0530

    Added: Main file and NumberGame class

--- number_game/NumberGame.java
+++ number_game/NumberGame.java
@@ -1,6 +1,16 @@
 import java.util.Random;
-private final class NumberGame{
-    public static void main(String[] args) {
+final class NumberGame{
+    Random random = new Random();
+    //By default, it ranges from 0 to 99 adding 1 to change its scope to 1 to 100
+    int randomNum = random.nextInt(100) + 1;
+    boolean correctGuess;
+    void compare(int num){
+        String output = (num==randomNum) ? "Congratulations! Your Guess is correct!" :
+                (Math.abs(num-randomNum)>=50) ? "Your guess is too far off!" :
+                        (Math.abs(num-randomNum)>10) ? "Your guess is out of the range of 20!" :
+                                "Your guess is within the range of 20!";
 
+        correctGuess = num == randomNum;
+        System.out.println(output);
     }
 }
